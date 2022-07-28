874a2cd0ed7e197734f181d95006531bb54b44d5
Josh <joshmiranda.exe@gmail.com>
2022-07-28 15:39:24 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
+    
   );
 }
 
